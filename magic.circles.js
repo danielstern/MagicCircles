171d41f5211ffe3432839698ede3dcf06a4d30e7
unknown <daniel@danielstern.ca>
2014-06-03 16:00:54 -0400

    multiple trippy text!!

--- magic.circles.js
+++ magic.circles.js
@@ -18,51 +18,31 @@ function circle(selector, radius) {
 
 }
 
-function rune(selector, radius, text) {
+function rune(selector, radius, text, fontSize) {
 
     var width = $(selector).width();
     var height = $(selector).height();
 
-//    var radius = width / 2;
-
     var svg = d3.select(selector)
     	.select("svg")
 
-    // svg
-    // // .append("g")
-    // //     .attr("x",15)
-    // //     .attr("y",150)
-    // .append('path')
-    // .attr("d","M 10,90 Q 100,15 200,70 Q 340,140 400,30")
-    // .attr("id","cs")
-    // .attr("fill","none")
-    // .attr("stroke","blue")
-    // .attr("strokeWidth",1);
-
-    // svg
-    // .append("text")
-    //     .text(text)
-    //     .attr("y",150)
-    //     .attr("x",150)
-    //     .append("textPath")
-    //     	.attr("xlink:href", "#cs")
-
+    var runeId = lol.guid();
 
     svg.append("defs").append("path")
-    .attr("id", "s3")
+    .attr("id", "s3"+runeId)
     .attr("d", "M 0,-1   C 0.5523, -1   1, -0.5523    1,0  C 1, 0.5523    0.5523, 1     0,1  C -0.5523, 1   -1, 0.5523    -1,0         C -1, -0.5523  -0.5523, -1   0,-1")
      .attr("transform","translate("+width/2+","+height/2+") scale("+radius+","+radius+")")
     // .attr("scale",150)
 
     var thing = svg.append("g")
-        .attr("id", "thing")
+        .attr("id", "thing"+runeId)
         .style("fill", "navy");
      
     thing.append("text")
-        .style("font-size", "20px")
+        .style("font-size", fontSize||20+"px")
       .append("textPath")
-        .attr("xlink:href", "#s3")
-        .text("Wavy text is the gimmick for many years to come (d3)");
+        .attr("xlink:href", "#s3"+runeId)
+        .text(text);
      
     thing.append("use")
         .attr("xlink:href", "#s3")
@@ -81,4 +61,6 @@ circle("#circle1", 85);
 circle("#circle1", 50);
 circle("#circle1", 45);
 
-rune("#circle1",85,"ω2 = ω02 + 2α(θ − θ0)");
+rune("#circle1",45,"ω2 = ω02 + 2α(θ − θ0)",20);
+rune("#circle1",85,"ω2 = ω02 + 2α(θ − θ0)",20);
+
