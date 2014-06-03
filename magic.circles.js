3dd2722e830ce8434f30b7ca77b7f86e46ba9b75
unknown <daniel@danielstern.ca>
2014-06-03 14:48:12 -0400

    positioned text

--- magic.circles.js
+++ magic.circles.js
@@ -1,43 +1,49 @@
-function circle(selector,radius) {
+function circle(selector, radius) {
 
-	var width = 500;
-	height = 500;
-	svg = d3.select(selector);
+    var width = 500;
+    height = 500;
+    svg = d3.select(selector)
+    	.select("svg")
 
-	var frame = $(selector).find('svg')[0];
-
-	console.log("Drawin circle");
-	if (!frame) {
-
-	    var svg = frame || d3.select(selector)
-	        .append("svg")
-	        .attr("class","main")
-	        .attr("width", width)
-	        .attr("height", height)
-	       
-
-	    }   else {
-	        var svg = d3.select(selector).select(".main");
-	    }
-
-	    svg .append("circle")
-	        .attr("r",0)
-	        .attr("cx",width/2)
-	        .attr("cy",height/2)
-	        .attr("stroke","#000")
-	        .attr("fill","none")
-	        .attr("stroke-width",3)
-	        	.transition()
-	        	.duration(500)
-	        	.attr("r",radius)
+    svg.append("circle")
+        .attr("r", 0)
+        .attr("cx", width / 2)
+        .attr("cy", height / 2)
+        .attr("stroke", "#000")
+        .attr("fill", "none")
+        .attr("stroke-width", 3)
+        .transition()
+        .duration(500)
+        .attr("r", radius)
 
 }
 
-circle("#circle1",150);
-circle("#circle1",145);
+function rune(selector, text) {
 
-circle("#circle1",100);
-circle("#circle1",85);
+    var width = 500;
+    height = 500;
+    var svg = d3.select(selector)
+    	.select("svg")
 
-circle("#circle1",50);
-circle("#circle1",45);
+    svg
+    // .append("g")
+    //     .attr("x",15)
+    //     .attr("y",150)
+    .append("text")
+        .text(text)
+        .attr("y",150)
+        .attr("x",150)
+
+}
+
+
+circle("#circle1", 150);
+circle("#circle1", 145);
+
+circle("#circle1", 100);
+circle("#circle1", 85);
+
+circle("#circle1", 50);
+circle("#circle1", 45);
+
+rune("#circle1","ω2 = ω02 + 2α(θ − θ0)");
