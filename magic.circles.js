72fb87055a99a7b2e9509371dd1a6535a429ad09
unknown <daniel@danielstern.ca>
2014-06-04 06:38:44 -0400

    added drop shadow!!!

--- magic.circles.js
+++ magic.circles.js
@@ -3,15 +3,19 @@ function circle(selector, radius) {
     var width = 500;
     height = 500;
     svg = d3.select(selector)
-        .select("svg")
+        .select("svg");
 
-    svg.append("circle")
+    var circle = svg.append("circle");
+
+    circle
         .attr("r", 0)
         .attr("cx", width / 2)
         .attr("cy", height / 2)
         .attr("stroke", "#000")
         .attr("fill", "none")
-        .attr("stroke-width", 3)
+        .attr("stroke-width", radius / 100)
+
+    circle
         .transition()
         .duration(500)
         .attr("r", radius)
@@ -29,43 +33,44 @@ function circleRing(selector, radius, count, innerRadius, reverse) {
         .select("svg")
     var g = svg.append("g")
 
-        for (var i = 0; i < count; i++) {
+    for (var i = 0; i < count; i++) {
 
-            var completeness = i / count;
-            var q = 1;
+        var completeness = i / count;
+        var q = 1;
 
-            // g
-              //.attr("transform","translate(500,500)");
+        var circle = g.append("circle");
+        circle
+            .attr("r", 0)
+            .attr("cx", width / 2 + (Math.cos((offset + completeness) * RAD)) * q * radius)
+            .attr("cy", height / 2 + (Math.sin((offset + completeness) * RAD)) * q * radius)
+            .attr("stroke", "gray")
+            .attr("fill", "none")
+            .attr("stroke-width", innerRadius / 5);
 
-
-			var circle = g.append("circle");
-            circle
-			    .attr("r", innerRadius)
-			    .attr("cx", width / 2 + (Math.cos((offset + completeness) * RAD) )* q * radius )
-                .attr("cy", height / 2 + (Math.sin((offset + completeness) * RAD)) * q * radius)
-			    .attr("stroke", "gray")
-			    .attr("fill", "none")
-			    .attr("stroke-width", innerRadius / 5);
-			//     .transition()
-			
-
-
-        }
-         setInterval(animate, 100,g);
-
-        function animate(thing) {
-               offset = (reverse) ? offset - 1 : offset + 1;
-
-                thing
-                    .transition()
-                    .ease("linear")
-                    .duration(100)
-                    .attr("transform","rotate("+offset+", 250, 250)");
-
-        }
+        circle
+            .transition()
+            .duration(500)
+            .attr("r", innerRadius)
 
 
-    
+
+    }
+
+    setInterval(animate, 100, g);
+
+    function animate(ring) {
+        offset = (reverse) ? offset - 1 : offset + 1;
+
+        ring
+            .transition()
+            .ease("linear")
+            .duration(100)
+            .attr("transform", "rotate(" + offset + ", 250, 250)");
+
+    }
+
+
+
 
 }
 
@@ -85,50 +90,85 @@ function rune(selector, radius, text, fontSize, reverse) {
     path
         .attr("id", "s3" + runeId)
         .attr("d", "M 0,-1   C 0.5523, -1   1, -0.5523    1,0  C 1, 0.5523    0.5523, 1     0,1  C -0.5523, 1   -1, 0.5523    -1,0         C -1, -0.5523  -0.5523, -1   0,-1")
-        .attr("transform", "scale(" + radius + "," + radius + ")");
-        // .transition()
-        // .duration(5000)
-        // .ease("linear")
-        // .attr("transform", "scale(" + radius + "," + radius + ")");
+        .attr("transform", "scale(" + radius + "," + radius + ")")
+
 
 
     function animate() {
-    			rotation = (reverse) ? rotation - 179.9 : rotation + 179.9;
-    	    thing
-    	        .transition()
-    	        .duration(5000)
-    	        .ease("linear")
-    	        .attr("transform","translate(" + width / 2 + "," + height / 2 + ")  rotate("+rotation+")");
+        rotation = (reverse) ? rotation - 179.9 : rotation + 179.9;
+        ring
+            .transition()
+            .duration(5000)
+            .ease("linear")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")  rotate(" + rotation + ")");
 
     }
-    
+
     setInterval(animate, 5000);
 
 
-    var thing = svg.append("g")
-        .attr("id", "thing" + runeId)
-        .attr('transform',"translate(" + width / 2 + "," + height / 2 + ")  rotate("+rotation+")")
+    var ring = svg.append("g")
+        .attr("id", "ring" + runeId)
+        .attr('transform', "translate(" + width / 2 + "," + height / 2 + ")  rotate(" + rotation + ")")
 
-    thing.append("text")
+    ring.append("text")
         .style("font-size", fontSize || 20 + "px")
         .append("textPath")
         .attr("xlink:href", "#s3" + runeId)
-        .text(text);
+        .style("filter", "url(#drop-shadow)")
+        .text(text)
+        .attr("opacity", 0)
+        .transition()
+        .duration(400)
+        .ease("linear")
+        .attr("opacity", 1);
 
-    thing.append("use")
+    ring.append("use")
         .attr("xlink:href", "#s3" + runeId)
         .style("stroke", "none")
         .attr("text-rendering", "optimizeSpeed ")
         .style("fill", "none");
-    
-	    animate();
+
+    animate();
+
+    var defs = svg.append("defs");
+
+    var filter = defs.append("filter")
+        .attr("id", "drop-shadow")
+        .attr("height", "130%");
+
+    // SourceAlpha refers to opacity of graphic that this filter will be applied to
+    // convolve that with a Gaussian with standard deviation 3 and store result
+    // in blur
+    filter.append("feGaussianBlur")
+        .attr("in", "SourceAlpha")
+        .attr("stdDeviation", 5)
+        .attr("result", "blur");
+
+    // translate output of Gaussian blur to the right and downwards with 2px
+    // store result in offsetBlur
+    filter.append("feOffset")
+        .attr("in", "blur")
+        .attr("dx", 5)
+        .attr("dy", 5)
+        .attr("result", "offsetBlur");
+
+    // overlay original SourceGraphic over translated blurred opacity by using
+    // feMerge filter. Order of specifying inputs is important!
+    var feMerge = filter.append("feMerge");
+
+    feMerge.append("feMergeNode")
+        .attr("in", "offsetBlur")
+    feMerge.append("feMergeNode")
+        .attr("in", "SourceGraphic");
 
 }
 
+circle("#circle1", 180);
+circle("#circle1", 190);
 
 circle("#circle1", 150);
 circle("#circle1", 145);
-circle("#circle1", 180);
 
 circle("#circle1", 105);
 circle("#circle1", 85);
@@ -139,12 +179,14 @@ circle("#circle1", 45);
 circle("#circle1", 35);
 circle("#circle1", 25);
 
-circleRing("#circle1", 70, 6,15);
-circleRing("#circle1", 85, 40, 2);
- circleRing("#circle1", 195, 24,6);
+circleRing("#circle1", 70, 6, 15, true);
+circleRing("#circle1", 70, 18, 5);
+circleRing("#circle1", 85, 40, 2, true);
+circleRing("#circle1", 185, 72, 3);
 
+rune("#circle1", 27, "ηκε ολοσχερώς στο μυαλό των θεατών από τη φαντασ", 5, true);
 rune("#circle1", 45, "ηκε ολοσχερώς στο μυαλό των θεατών από τη φαντασ", 8, false);
-rune("#circle1", 90, "μέχρι το μυαλό της σε μια εμφάνιση του θάρρους. Όταν", 15,true);
+rune("#circle1", 90, "μέχρι το μυαλό της σε μια εμφάνιση του θάρρους. Όταν", 15, true);
 rune("#circle1", 115, "δάκρυ φάνηκε να σκοτεινιάζει τα μάτια της όταν μας είδε? αλλά ανάρρωσε γρήγορα τον εαυτό της ", 35);
-rune("#circle1", 160, "δάκρυ φάνηκε να σκοτεινιάζει τα μάτια της όταν μας είδε? αλλά ανάρρωσε γρήγορα τον εαυτό της ", 15,true);
+rune("#circle1", 160, "δάκρυ φάνηκε να σκοτεινιάζει τα μάτια της όταν μας είδε? αλλά ανάρρωσε γρήγορα τον εαυτό της ", 15, true);
 //rune("#circle1", 145, lol.hipster() + lol.hipster(), 7,true);
