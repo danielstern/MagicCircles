968b5dc01d52e0cb9a0eb694ea62c0dfdfabfdc1
unknown <daniel@danielstern.ca>
2014-06-04 17:48:40 -0400

    tightened filters

--- magic.circles.js
+++ magic.circles.js
@@ -1,7 +1,7 @@
-var MagicCircle = function(selector, animationSpeed) {
+var MagicCircle = function(selector) {
 
     // CONSTS
-    var RAD = Math.PI * 2;    
+    var RAD = Math.PI * 2;
     var magicCircle = this;
     var width = $(selector).width();
     var height = $(selector).height();
@@ -11,9 +11,6 @@ var MagicCircle = function(selector, animationSpeed) {
     var defs = svg.append("defs");
 
     // user settings    
-    var introAnimationTime = 777;
-    var animationSpeed = animationSpeed || 5;
-
 
     // initializers
     this.draw = {};
@@ -28,13 +25,55 @@ var MagicCircle = function(selector, animationSpeed) {
         }
     }
 
-    var colors = {
-        ring: "#182645",
-        text: "#243a6c",
-        smallRing: "#304f93",
+    this.styles = {
+        colors: {
+            ring: "#182645",
+            text: "#243a6c",
+            smallRing: "#304f93",
+        },
+        type: {
+            leading: 6,
+            typecase: "uppercase"
+        },
+        animation: {
+            inSpeed: 666,
+            animationSpeed:6.66
+        },
+        graphics: {
+            blur: {
+                level: 0
+            },
+            shadow: {
+                level: 1,
+                distance: 0
+            }
+        }
     }
 
-    function init() {
+    this.init = function() {
+
+        var blurFilter = defs.append("filter")
+            .attr("id", "drop-blur")
+            .attr("height", "130%")
+
+        blurFilter.append("feGaussianBlur")
+            .attr("in", "SourceAlpha")
+            .attr("stdDeviation", magicCircle.styles.graphics.blur.level)
+            .attr("result", "blur");
+
+        blurFilter.append("feOffset")
+            .attr("in", "blur")
+            .attr("dx", 0)
+            .attr("dy", 0)
+            .attr("result", "offsetBlur");
+
+        var blurFeMerge = blurFilter.append("feMerge");
+
+        blurFeMerge.append("feMergeNode")
+            .attr("in", "offsetBlur")
+        blurFeMerge.append("feMergeNode")
+            .attr("in", "SourceGraphic");
+
 
         var shadowFilter = defs.append("filter")
             .attr("id", "drop-shadow")
@@ -42,26 +81,34 @@ var MagicCircle = function(selector, animationSpeed) {
 
         shadowFilter.append("feGaussianBlur")
             .attr("in", "SourceAlpha")
-            .attr("stdDeviation", 4)
+            .attr("stdDeviation", magicCircle.styles.graphics.shadow.level)
             .attr("result", "blur");
 
         shadowFilter.append("feOffset")
             .attr("in", "blur")
-            .attr("dx", 0)
-            .attr("dy", 0)
+            .attr("dx", magicCircle.styles.graphics.shadow.distance)
+            .attr("dy", magicCircle.styles.graphics.shadow.distance)
             .attr("result", "offsetBlur");
 
-        var feMerge = shadowFilter.append("feMerge");
+        shadowFilter.append("feComponentTransfer")
+            .append("feFuncA")
+                .attr("type","linear")
+                .attr("slope","0.2")
 
-        feMerge.append("feMergeNode")
+        var shadowFeMerge = shadowFilter.append("feMerge");
+
+        shadowFeMerge.append("feMergeNode")
             .attr("in", "offsetBlur")
-        feMerge.append("feMergeNode")
+            .attr("alpha", "0.1")
+        shadowFeMerge.append("feMergeNode")
             .attr("in", "SourceGraphic");
 
         setInterval(magicCircle.animate, 100);
+
+        this.init = null;
     };
 
-    init();
+    // init();
 
     // methods
     this.draw.circle = function(radius) {
@@ -72,14 +119,14 @@ var MagicCircle = function(selector, animationSpeed) {
             .attr("r", 0)
             .attr("cx", width / 2)
             .attr("cy", height / 2)
-            .attr("stroke", colors.ring)
+            .attr("stroke", magicCircle.styles.colors.ring)
             .attr("fill", "none")
             .style("filter", "url(#drop-shadow)")
             .attr("stroke-width", radius / 100)
 
         circle
             .transition()
-            .duration(introAnimationTime)
+            .duration(magicCircle.styles.animation.inSpeed)
             .attr("r", radius)
 
     }
@@ -100,26 +147,26 @@ var MagicCircle = function(selector, animationSpeed) {
                 .attr("r", 0)
                 .attr("cx", width / 2 + (Math.cos((offset + completeness) * RAD)) * q * radius)
                 .attr("cy", height / 2 + (Math.sin((offset + completeness) * RAD)) * q * radius)
-                .attr("stroke", colors.smallRing)
+                .attr("stroke", magicCircle.styles.colors.smallRing)
                 .attr("fill", "none")
-                .style("filter", "url(#drop-shadow)")
+                .style("filter", "url(#drop-blur)")
                 .attr("stroke-width", 0.5 + innerRadius / 15);
 
             circle
                 .transition()
-                .duration(introAnimationTime)
+                .duration(magicCircle.styles.animation.inSpeed)
                 .attr("r", innerRadius)
 
 
         }
 
-        magicCircle.onanimate(function(){
+        magicCircle.onanimate(function() {
             offset = (reverse) ? offset - 1 * (speed || animationSpeed) : offset + 1 * (speed || animationSpeed);
             ring
                 .transition()
                 .ease("linear")
                 .duration(100)
-                .attr("transform", "rotate(" + offset + ", "+width/2+", "+width/2+")");
+                .attr("transform", "rotate(" + offset + ", " + width / 2 + ", " + width / 2 + ")");
         });
     }
 
@@ -136,7 +183,7 @@ var MagicCircle = function(selector, animationSpeed) {
             .attr("transform", "scale(" + radius + "," + radius + ")")
 
 
-        magicCircle.onanimate(function(){
+        magicCircle.onanimate(function() {
             rotation = (reverse) ? rotation - 1 * (speed || animationSpeed) : rotation + 1 * (speed || animationSpeed);
             ring
                 .transition()
@@ -151,17 +198,19 @@ var MagicCircle = function(selector, animationSpeed) {
             .attr('transform', "translate(" + width / 2 + "," + height / 2 + ")  rotate(" + rotation + ")")
 
         ring.append("text")
-            .style("font-size", fontSize || 20 + "px")
+            .style("font-size", fontSize + "px")
             .append("textPath")
             .attr("xlink:href", "#s3" + runeId)
-            .style("filter", "url(#drop-shadow)")
+            .style("letter-spacing", magicCircle.styles.type.leading)
+            .style( "text-transform", magicCircle.styles.type.typecase)
+            .style("filter", "url(#drop-blur)")
             .text(text)
             .attr("opacity", 0)
-            .attr("fill", colors.text)
+            .attr("fill", magicCircle.styles.colors.text)
             .transition()
-            .duration(introAnimationTime)
+            .duration(magicCircle.styles.animation.inSpeed)
             .ease("linear")
-            .attr("opacity", 1);
+             .attr("opacity", 1);
 
         ring.append("use")
             .attr("xlink:href", "#s3" + runeId)
@@ -177,28 +226,30 @@ var MagicCircle = function(selector, animationSpeed) {
 
         var draw = this.draw;
 
+        this.init();
+
         return {
             currentRadius: 0,
             selector: selector,
-            ring:function(radius){
+            ring: function(radius) {
                 draw.circle(this.currentRadius);
                 if (radius) return this.space(radius);
                 return this;
             },
-            circleRing:function(count,innerRadius,speed,reverse) {
+            circleRing: function(count, innerRadius, speed, reverse) {
                 draw.circleRing(this.currentRadius + innerRadius, count, innerRadius, speed, reverse);
                 this.currentRadius += innerRadius * 2;
                 return this;
             },
-            space:function(length){
+            space: function(length) {
                 this.currentRadius += length;
                 return this;
             },
-            backspace:function(length){
+            backspace: function(length) {
                 this.currentRadius -= length;
                 return this;
             },
-            text:function(height,text,speed,reverse){
+            text: function(height, text, speed, reverse) {
                 var padding = 2;
                 draw.runeRing(this.currentRadius + padding, text, height, speed || 10, reverse);
                 this.currentRadius += height;
@@ -215,6 +266,10 @@ var MagicCircle = function(selector, animationSpeed) {
 var magic = new MagicCircle("#circle1", 2);
 var magic2 = new MagicCircle("#circle2", 2);
 
+magic.styles.colors.ring = "#8091d4"
+magic.styles.colors.smallRing = "#bfc8ea"
+magic.styles.colors.text = "#9facdf"
+
 
 magic.cast()
     .ring(10)
@@ -222,25 +277,26 @@ magic.cast()
     .ring(5)
     .ring(5)
     .ring(5)
-    .text(15, lol.hipster(),10, true)
+    .text(15, lol.hipster(), 10, true)
     .ring(5)
     .ring(5)
     .space(10)
     .ring()
     .space(5)
     .ring()
-    .circleRing(10,5,10,true)
+    .circleRing(10, 5, 10, true)
     .ring()
-    .text(15, lol.hipster(),10, true)
+    .text(15, lol.hipster(), 10, true)
     .ring()
-    .circleRing(10,5,10)
+    .circleRing(10, 5, 10)
     .ring()
     .space(2)
-    .circleRing(3,21,10)
+    .circleRing(3, 21, 10)
     .space(2)
     .ring()
     .backspace(10)
-    .text(10, lol.hipster(),10, false)
+    .ring()
+    .text(10, lol.hipster(), 10, false)
     .backspace(30)
     .ring(5)
     .ring(5)
