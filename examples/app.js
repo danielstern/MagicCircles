7df115468b31c98747143751d55d0c1d0921cf09
unknown <daniel@danielstern.ca>
2014-06-08 16:14:39 -0400

    refactor working good!

--- examples/app.js
+++ examples/app.js
@@ -6,6 +6,14 @@ angular.module("MagicCirclesDemo", ['ngRoute'])
         templateUrl: 'Casting/casting.html',
         controller: 'Casting'
       }).
+      when('/styling', {
+        templateUrl: 'Styling/styling.html',
+        controller: 'Styling'
+      }).
+      when('/interactivity', {
+        templateUrl: 'Interactivity/interactivity.html',
+        controller: 'Interactivity'
+      }).
       otherwise({
         redirectTo: '/home'
       });
