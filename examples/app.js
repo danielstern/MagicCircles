49672f90e2c3893441ace9fb089463b7a1ea8e0b
unknown <daniel@danielstern.ca>
2014-06-08 16:04:52 -0400

    first view working!!!

--- examples/app.js
+++ examples/app.js
@@ -1,1 +1,12 @@
-angular.module("MagicCirclesDemo", []);
+angular.module("MagicCirclesDemo", ['ngRoute'])
+.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/casting', {
+        templateUrl: 'Casting/casting.html',
+        controller: 'Casting'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }]);
